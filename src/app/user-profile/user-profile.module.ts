853a373c156e61227fb87b695c995d96c2bf5fdc
Mike Ekert <mikeekert@gmail.com>
2018-02-21 11:48:13 -0600

    added app module, components

---
+++ src/app/user-profile/user-profile.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailsComponent } from './details/details.component';
+import { UserProfileComponent } from './user-profile.component';
+import { LocationsComponent } from './locations/locations.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DetailsComponent, UserProfileComponent, LocationsComponent]
+})
+export class UserProfileModule { }
