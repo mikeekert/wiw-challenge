ac21803b1648aa682e32a6339941c3c0af3db2b9
Mike Ekert <mikeekert@gmail.com>
2018-02-27 14:53:56 -0600

    light jasmine testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { UserProfileModule } from './user-profile/user-profile.module';
@@ -8,7 +9,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule, UserProfileModule
+        RouterTestingModule
       ],
       declarations: [
         AppComponent, NavComponent
--- src/app/services/wiw_apiResponse.service.spec.ts
+++ src/app/services/wiw_apiResponse.service.spec.ts
@@ -1,12 +1,11 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { ApiService } from './wiw_apiResponse.service';
+import {TestBed, inject} from '@angular/core/testing';
+import {ApiService} from './wiw_apiResponse.service';
+import {HttpClient, HttpHeaders, HttpHandler} from '@angular/common/http';
 
 describe('apiService', () => {
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ApiService]
-    });
+    TestBed.configureTestingModule({providers: [ApiService, HttpClient, HttpHandler]});
   });
 
   it('should be created', inject([ApiService], (service: ApiService) => {
